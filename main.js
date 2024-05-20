aa0df8ddcd9d56de364a834400cc3233c2b4f9d1
Mohamed ghazoul <126819472+mohamedghazoul@users.noreply.github.com>
2024-05-20 12:13:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ window.onload = function(){
         // console.log(window.innerWidth)
         setTimeout(() => {
             // container.style.boxShadow = "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"
-            container.style.background = "rgb(240, 237, 237)"
+            container.style.background = "#fff"
         }, 2000);
     }else {
         img.style.right = "10%"
